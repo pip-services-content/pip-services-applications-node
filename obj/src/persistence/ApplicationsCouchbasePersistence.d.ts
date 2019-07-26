aa2cb717bd6aacbfdd4fa5455ab1499b15da4eac
pipdeveloper <pipdevs@gmail.com>
2019-07-26 22:00:25 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/ApplicationsCouchbasePersistence.d.ts
+++ obj/src/persistence/ApplicationsCouchbasePersistence.d.ts
@@ -6,6 +6,6 @@ import { ApplicationV1 } from '../data/version1/ApplicationV1';
 import { IApplicationsPersistence } from './IApplicationsPersistence';
 export declare class ApplicationsCouchbasePersistence extends IdentifiableCouchbasePersistence<ApplicationV1, string> implements IApplicationsPersistence {
     constructor();
-    private composeFilter;
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: (err: any, page: DataPage<ApplicationV1>) => void): void;
 }
